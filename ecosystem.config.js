856e666e3d0237c69edbfa64524cecd5076e46c2
Taylor Mosquera Castro <thaylor0184@hotmail.com>
2024-10-25 18:17:54 -0500

    Agregar ecosistema.config.js y actualizar configuración de ESLint para ignorar el archivo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-	apps: [
-		{
-			name: "APPTESTGENERATOR", // Nombre de la app, puedes ponerle el que quieras
-			script: "./dist/src/index.js", // El script que corre tu servidor Next.js
-			autorestart: true,
-			exec_mode: "cluster",
-			env: {
-				NODE_ENV: "production", // Define las variables de entorno
-			},
-		},
-	],
+  apps: [
+    {
+      name: "APPTESTGENERATOR",
+      script: "./dist/src/index.js",
+      interpreter: "node", // Asegúrate de que esté configurado
+      autorestart: true,
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
