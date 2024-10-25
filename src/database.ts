828c2d5269ec32368f61c7199c91b5d28cb06376
Taylor Mosquera Castro <thaylor0184@hotmail.com>
2024-10-25 17:12:12 -0500

    Agregar carga de variables de entorno para la conexión a la base de datos

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 // Reemplaza esto con tu URI de conexión desde MongoDB Atlas
 const MONGO_URI = process.env.MONGO_URI || '';
 
